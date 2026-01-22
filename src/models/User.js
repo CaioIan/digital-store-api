f966152d5c7d35a5d80845b61da7378708ace6d1
Caio Ian <iancaio015@gmail.com>
2026-01-21 21:54:41 -0300

    feat(database): cria tabela usuários com timestamps

---
+++ src/models/User.js
@@ -0,0 +1,50 @@
+
+const bcrypt = require('bcrypt');
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database/connection');
+
+
+const User = sequelize.define('User', {
+    id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false
+  },
+  firstname: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  surname: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+}, {
+  tableName: 'users',
+  hooks: {
+    beforeCreate: async (user) => {
+      if (user.password) {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+      }
+    },
+
+    beforeUpdate: async (user) => {
+      if (user.changed('password')) {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+      }
+    }
+  }
+});
+
+module.exports = User;
